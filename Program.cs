14af53a32aa42683ee9803c226ab5f3fb705e1bf
horvathl <horvathl@310-17>
2026-02-16 10:18:33 +0100

    2. feldat json

--- Program.cs
+++ Program.cs
@@ -1,26 +1,57 @@
-﻿using System.Text.Json;
+﻿
+using System.Text.Json;
 
-namespace Json_
+Emil Koleszár
+10:14(1 perccel ezelőtt)
+címzett: én
+
+using System.Text.Json;
+
+namespace ConsoleApp3
 {
     class Adat
     {
-
         public List<string> nevek { get; set; }
+        public List<int> korok { get; set; }
     }
+
+    class Diak
+    {
+        public String nev { get; set; }
+        public List<int> jegyek { get; set; }
+    }
+
     internal class Program
     {
-
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
-            String fajl = File.ReadAllText("adatok.json", System.Text.Encoding.Latin1);
+            String fajl = File.ReadAllText("Adat.json", System.Text.Encoding.Latin1);
             Console.WriteLine(fajl);
-            Adat adat=JsonSerializer.Deserialize<Adat>(fajl);
+            Adat adat = JsonSerializer.Deserialize<Adat>(fajl);
             foreach (var nev in adat.nevek)
-            { 
-            Console.WriteLine(nev);
+            {
+                Console.WriteLine(nev);
             }
-            //első életkor
-            Console.WriteLine($"{adat.nevek[0]} életkora:{adat.korok[0]}");
+            //első eletkor
+            Console.WriteLine($" {adat.nevek[0]} életkor: {adat.korok[0]}");
+
+            //diakok.json
+            fajl = File.ReadAllText("diakok.json", System.Text.Encoding.Latin1);
+            Console.WriteLine(fajl);
+            List<Diak> diakok = JsonSerializer.Deserialize<List<Diak>>(fajl);
+            Console.WriteLine("Keresett név:");
+            String neve = Console.ReadLine();
+            bool megvan = false;
+            foreach (var diak in diakok)
+            {
+                if (diak.nev == neve)
+                {
+                    Console.WriteLine("Átlag:" + diak.jegyek.Average());
+                    megvan = true;
+                }
+            }
+            if (megvan == false)
+                Console.WriteLine("Nincs ilyen nevű diák!");
         }
     }
 }
