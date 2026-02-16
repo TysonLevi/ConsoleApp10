56e66cdcc0fb657c7f822e10fa085fb53f700c62
horvathl <horvathl@310-17>
2026-02-16 09:36:02 +0100

    1. feladat: Adatok.json

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ConsoleApp10
+﻿using System.Text.Json;
+
+namespace Json_
 {
+    class Adat
+    {
+
+        public List<string> nevek { get; set; }
+    }
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void Main (string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String fajl = File.ReadAllText("adatok.json", System.Text.Encoding.Latin1);
+            Console.WriteLine(fajl);
+            Adat adat=JsonSerializer.Deserialize<Adat>(fajl);
+            foreach (var nev in adat.nevek)
+            { 
+            Console.WriteLine(nev);
+            }
+            //első életkor
+            Console.WriteLine($"{adat.nevek[0]} életkora:{adat.korok[0]}");
         }
     }
 }
